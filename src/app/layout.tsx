15ffe4b7674aa896bdadebddf3091dae3e0319ad
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-03-27 22:54:20 +0530

    Initial favicon and styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Geist, Instrument_Sans, Hanken_Grotesk } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
 
@@ -11,16 +11,20 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
+const instrument = Instrument_Sans({
   subsets: ["latin"],
-  variable: "--font-geist-sans",
+  variable: "--font-instrument",
+})
+const hanken = Hanken_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-hk",
 });
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" className={`${instrument.variable} ${hanken.variable}`}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
