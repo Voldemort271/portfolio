7532cdab2a14eca8eb6b254c3809bafe3cfe0d09
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-04-27 11:43:27 +0530

    Added content containers in /about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,9 +9,9 @@ const Page = () => {
         Some things about me that you may or may not like. Thank you for your
         cooperation.
       </TitleText>
-      <div className="flex h-full w-full items-end justify-between gap-12 px-12 py-5">
+      <div className="flex h-full w-full items-end justify-between gap-12 px-5 py-5 sm:px-12">
         <AboutContent />
-        <div className="aspect-[3/4] h-full max-h-[600px] bg-zinc-950/[0.2]"></div>
+        <div className="hidden aspect-[3/4] h-full max-h-[400px] bg-zinc-950/[0.2] md:block xl:max-h-[600px]"></div>
       </div>
     </main>
   );
--- src/components/about/about-content.tsx
+++ src/components/about/about-content.tsx
@@ -25,9 +25,28 @@ const AboutContent: React.FC = () => {
   }, []);
 
   return (
-    <div ref={wrapper} className="h-full w-full overflow-hidden">
+    <div ref={wrapper} className="h-full w-full overflow-hidden bg-red-200">
       <div ref={content} className="h-full w-full">
-        <div className="h-full w-full">hi</div>
+        <div className="flex h-full w-full flex-col items-start justify-end gap-12">
+          <div className="hidden aspect-[3/4] h-full max-h-[400px] self-end bg-zinc-950/[0.2] sm:block md:hidden"></div>
+          <div className="font-body max-w-[700px] space-y-5 text-base leading-normal text-zinc-950">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Architecto doloremque eum perspiciatis quam quia sit? Aut ipsa
+              nihil perferendis quidem quos! Aspernatur consequatur debitis
+              laborum, libero magnam neque obcaecati! Reiciendis!
+            </p>
+            <p className="hidden xl:block">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Architecto doloremque eum perspiciatis quam quia sit? Aut ipsa
+              nihil perferendis quidem quos! Aspernatur consequatur debitis
+              laborum, libero magnam neque obcaecati! Reiciendis!
+            </p>
+            <div className="font-body text-sm font-bold text-zinc-500 uppercase">
+              scroll
+            </div>
+          </div>
+        </div>
         <div className="grid grid-cols-12 items-end justify-items-start gap-y-5">
           {Array.from({ length: 5 }).map((_, i) => (
             <div
