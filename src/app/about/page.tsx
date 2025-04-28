158236c7e873b37b223a5fec298789f7f2b28ce3
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-04-28 19:17:19 +0530

    Added animation to image based on visible section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import TitleText from "@/components/about/title-text";
 import AboutContent from "@/components/about/about-content";
 import AboutImage from "@/components/about/about-image";
 
 const Page = () => {
   const [current, setCurrent] = useState(0);
-
-  useEffect(() => {
-    console.log(current);
-  }, [current]);
 
   return (
     <main className="font-body relative z-0 flex h-dvh w-screen flex-col items-stretch justify-between pt-20">
@@ -20,7 +16,7 @@ const Page = () => {
       </TitleText>
       <div className="flex h-full w-full items-end justify-between gap-12 px-5 py-5 sm:px-12">
         <AboutContent setCurrent={setCurrent} />
-        <AboutImage />
+        <AboutImage current={current} />
       </div>
     </main>
   );
--- src/components/about/about-image.tsx
+++ src/components/about/about-image.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+"use client";
 
-const AboutImage = () => {
+import React from "react";
+import { AnimatePresence, motion } from "motion/react";
+
+const DisplayImage = ({ current }: { current: number }) => {
   return (
-    <div className="hidden aspect-[3/4] h-full max-h-[400px] bg-zinc-950/[0.2] md:block xl:max-h-[600px]"></div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        transition: { duration: 0.7, ease: [0.33, 1, 0.68, 1] },
+      }}
+      exit={{
+        opacity: 0,
+        y: -50,
+        transition: { duration: 0.7, ease: [0.32, 0, 0.67, 0] },
+      }}
+      className="h-full w-full bg-zinc-950/[0.2]"
+    >
+      {current}
+    </motion.div>
+  );
+};
+
+const AboutImage = ({ current }: { current: number }) => {
+  return (
+    <div className="hidden aspect-[3/4] h-full max-h-[400px] md:block xl:max-h-[600px]">
+      <AnimatePresence mode="wait">
+        <DisplayImage current={current} key={current} />
+      </AnimatePresence>
+    </div>
   );
 };
 
